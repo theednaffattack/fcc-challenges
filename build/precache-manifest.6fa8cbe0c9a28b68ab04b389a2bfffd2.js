6cbc89717a7d45f492682faf054111369ea98dbc
eddie naff <eddienaff@eddies-i7-MBP.local>
2019-02-19 02:24:22 -0800

    deploy to heroku

---
+++ build/precache-manifest.6fa8cbe0c9a28b68ab04b389a2bfffd2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ba4ec97ac680712581",
+    "url": "/static/css/main.80932c1f.chunk.css"
+  },
+  {
+    "revision": "82ba4ec97ac680712581",
+    "url": "/static/js/main.82ba4ec9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7343feb9c23131565bce",
+    "url": "/static/css/2.3eb117b2.chunk.css"
+  },
+  {
+    "revision": "7343feb9c23131565bce",
+    "url": "/static/js/2.7343feb9.chunk.js"
+  },
+  {
+    "revision": "3946f9fe73921f85ce63735196183592",
+    "url": "/static/media/Chord_2.3946f9fe.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1cfea05e9aeb27be5b041e40c1ca80f",
+    "url": "/static/media/Heater-1.b1cfea05.mp3"
+  },
+  {
+    "revision": "0d219666c08f6037c2f4c6ccfa8c71bb",
+    "url": "/static/media/Heater-2.0d219666.mp3"
+  },
+  {
+    "revision": "632e877262affcec87e77ef0a409969c",
+    "url": "/static/media/Heater-3.632e8772.mp3"
+  },
+  {
+    "revision": "d9de8a500f2556463e3a3784274f1847",
+    "url": "/static/media/Heater-4_1.d9de8a50.mp3"
+  },
+  {
+    "revision": "3062daca6b46c54601c994dcdda8ef6b",
+    "url": "/static/media/Heater-6.3062daca.mp3"
+  },
+  {
+    "revision": "abe03961c7511cc994b64bcebc93b0f2",
+    "url": "/static/media/Cev_H2.abe03961.mp3"
+  },
+  {
+    "revision": "5c9f80876a2f7c9c7dd4dba4a1784f95",
+    "url": "/static/media/Dsc_Oh.5c9f8087.mp3"
+  },
+  {
+    "revision": "200b4bd34ad53416203739f8ab241e3b",
+    "url": "/static/media/Kick_n_Hat.200b4bd3.mp3"
+  },
+  {
+    "revision": "09fda25678eb5321a1b3d456bda527e1",
+    "url": "/static/media/RP4_KICK_1.09fda256.mp3"
+  },
+  {
+    "revision": "d8544f3e90cfa0d7ed4526b77e93408b",
+    "url": "/static/media/Chord_1.d8544f3e.mp3"
+  },
+  {
+    "revision": "561c9a5450dd4aef32292a06f357d553",
+    "url": "/static/media/Give_us_a_light.561c9a54.mp3"
+  },
+  {
+    "revision": "3fa8228b5b7459101ac9f7d345244b11",
+    "url": "/static/media/Dry_Ohh.3fa8228b.mp3"
+  },
+  {
+    "revision": "484073ffad6651504fefdb97b111ef2e",
+    "url": "/static/media/Bld_H1.484073ff.mp3"
+  },
+  {
+    "revision": "a6a991c55f20e9c7a390e86a2d6e4491",
+    "url": "/static/media/punchy_kick_1.a6a991c5.mp3"
+  },
+  {
+    "revision": "123a99c7ed11a3254ac92032ffba1122",
+    "url": "/static/media/side_stick_1.123a99c7.mp3"
+  },
+  {
+    "revision": "cd861874f961c37c2ecec27f6fd20667",
+    "url": "/static/media/Brk_Snr.cd861874.mp3"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/static/media/BeepSound.988833bf.wav"
+  },
+  {
+    "revision": "492fef9d8022f1b80ce46574bce27a03",
+    "url": "/index.html"
+  }
+];
